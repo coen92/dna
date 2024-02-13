7af1d2f59adc254b3d0ef4511eea4db79f43b4fe
Piotr Kapica <piotr.kapica@stepstone.com>
2024-02-13 19:15:38 +0100

    DNA-01: initial commit with subscription module

---
+++ subscription-module/src/main/java/com/coen92/subscriptionmodule/SubscriptionModuleApplication.java
@@ -0,0 +1,13 @@
+package com.coen92.subscriptionmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionModuleApplication.class, args);
+    }
+
+}
---
+++ subscription-module/src/test/java/com/coen92/subscriptionmodule/SubscriptionModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coen92.subscriptionmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
