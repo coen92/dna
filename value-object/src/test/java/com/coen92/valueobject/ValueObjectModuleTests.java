bf48fe9a7cd8db22c05a2d37e4b3dbb10aa3ff0b
Piotr Kapica <piotr.kapica@stepstone.com>
2024-03-03 18:22:39 +0100

    DNA-01: value-object module

---
+++ value-object/src/test/java/com/coen92/valueobject/ValueObjectModuleTests.java
@@ -0,0 +1,13 @@
+package com.coen92.valueobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueObjectModuleTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
