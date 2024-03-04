b4b64c4f7897a6050df79c5d8f54b3afe98ef079
coen92 <piotrkapica92@gmail.com>
2024-03-04 21:34:34 +0100

    DNA-01: extend value-object with validation of business rules

---
+++ value-object/src/main/java/com/coen92/valueobject/course/Course.java
@@ -0,0 +1,8 @@
+package com.coen92.valueobject.course;
+
+public class Course {
+    boolean isOnSite() {
+        // todo: implementation
+        return true;
+    }
+}
--- value-object/src/main/java/com/coen92/valueobject/course/CourseCapacity.java
+++ value-object/src/main/java/com/coen92/valueobject/course/CourseCapacity.java
@@ -1,10 +1,31 @@
 package com.coen92.valueobject.course;
 
+import static java.lang.StringTemplate.STR;
+
 public class CourseCapacity {
+    private static final int ON_SITE_COURSE_CAPACITY_LIMIT = 20;
     private final int capacity;
 
-    public CourseCapacity(int capacity) {
+    private CourseCapacity(int capacity) {
         this.capacity = capacity;
+    }
+
+    public static CourseCapacity of(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalStateException("Capacity cannot be a negative number");
+        }
+        return new CourseCapacity(capacity);
+    }
+
+    public static CourseCapacity of(int capacity, Course course) {
+        if (assertOnSiteCourseLimit(capacity, course)) {
+            throw new IllegalStateException(STR."On site courses can't have more than \{ON_SITE_COURSE_CAPACITY_LIMIT} participants!");
+        }
+        return of(capacity);
+    }
+
+    private static boolean assertOnSiteCourseLimit(int capacity, Course course) {
+        return course.isOnSite() && capacity > ON_SITE_COURSE_CAPACITY_LIMIT;
     }
 
     CourseCapacity withExtraSpace(CourseCapacity extraSpace) {
